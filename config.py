31f0ba23d35e312c16f715de6fdfb8fdc59e5d34
KaliusOs <mail.business.contact01@gmail.com>
2025-04-05 11:58:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+
 HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN", "")
 
 BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
